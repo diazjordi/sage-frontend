a4b6e604ed01c34085ce4d4a1ca699b15d19af57
Jordi Diaz <jordid91@gmail.com>
2014-08-08 13:01:44 -0700

    Table creation and population

--- js/myscript.js
+++ js/myscript.js
@@ -1,12 +1,61 @@
-/**
- * Created by jd5582 on 8/7/14.
- */
+/** * Created by jd5582 on 8/7/14. */
 
-function load() {
-    var mydata = JSON.parse(data);
-    var mydate = JSON.parse(date);
-    alert(mydata[0].name);
-    alert(mydata[0].age);
-    alert(mydate[0].name);
-    alert(mydate[0].age);
+
+function load(prod, cat, geo) {
+
+    // Find a <table> element with id="myTable":
+    var table = document.getElementById("table");
+    //Create an empty <tr> element and add it to the 1st position of the table:
+    var row = table.insertRow(0); var row2 = table.insertRow(1);
+    // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+    var cell7 = row.insertCell(6);
+    var cell8 = row.insertCell(7);
+    var cell9 = row.insertCell(8);
+    var cell10 = row.insertCell(9);
+    var cell11 = row.insertCell(10);
+    var cell12 = row2.insertCell(0);
+    var cell13 = row2.insertCell(1);
+    var cell14 = row2.insertCell(2);
+    var cell15 = row2.insertCell(3);
+    var cell16= row2.insertCell(4);
+    var cell17 = row2.insertCell(5);
+    var cell18 = row2.insertCell(6);
+    var cell19 = row2.insertCell(7);
+    var cell20 = row2.insertCell(8);
+    var cell21 = row2.insertCell(9);
+    var cell22 = row2.insertCell(10);
+    // Add some text to the new cells:
+    for(i = 0; i < json.length; i++){
+        if(json[i].prod == prod && json[i].cat == cat && json[i].geo == geo) {
+
+            cell1.innerHTML = (json[i].Field1);
+            cell2.innerHTML =(json[i].CH100);
+            cell3.innerHTML =(json[i].CH200 );
+            cell4.innerHTML =(json[i].CH300 );
+            cell5.innerHTML =(json[i].CH400 );
+            cell6.innerHTML =(json[i].CH500);
+            cell7.innerHTML =(json[i].CH600 );
+            cell8.innerHTML =(json[i].CH700);
+            cell9.innerHTML =(json[i].CH800);
+            cell10.innerHTML =(json[i].CH900);
+            cell11.innerHTML =(json[i].CH1000);
+            cell12.innerHTML =(json[i].Field2);
+            cell13.innerHTML =(json[i].CL100);
+            cell14.innerHTML =(json[i].CL200 );
+            cell15.innerHTML =(json[i].CL300 );
+            cell16.innerHTML =(json[i].CL400 );
+            cell17.innerHTML =(json[i].CL500 );
+            cell18.innerHTML =(json[i].CL600 );
+            cell19.innerHTML =(json[i].CL700);
+            cell20.innerHTML =(json[i].CL800);
+            cell21.innerHTML =(json[i].CL900);
+            cell22.innerHTML =(json[i].CL1000);
+        }
+    }
 }
--- js/tableCreation.js
+++ js/tableCreation.js
@@ -1,17 +1,35 @@
 /**
  * Created by jd5582 on 8/7/14.
  */
+$(document).ready(function () {
+var tr;
+for (var i = 0; i < json.length; i++) {
+    tr = $('<tr/>');
+    tr.append("<td>" + json[i].Field1 + "</td>");
+    tr.append("<td>" + json[i].CH100 + "</td>");
+    tr.append("<td>" + json[i].CH200 + "</td>");
+    tr.append("<td>" + json[i].CH300 + "</td>");
+    tr.append("<td>" + json[i].CH400 + "</td>");
+    tr.append("<td>" + json[i].CH500 + "</td>");
+    tr.append("<td>" + json[i].CH600 + "</td>");
+    tr.append("<td>" + json[i].CH700 + "</td>");
+    tr.append("<td>" + json[i].CH800 + "</td>");
+    tr.append("<td>" + json[i].CH900 + "</td>");
+    tr.append("<td>" + json[i].CH1000 + "</td>");
+    $('table').append(tr);
+    tr = $('<tr/>');
+    tr.append("<td>" + json[i].Field2 + "</td>");
+    tr.append("<td>" + json[i].CL100 + "</td>");
+    tr.append("<td>" + json[i].CL200 + "</td>");
+    tr.append("<td>" + json[i].CL300 + "</td>");
+    tr.append("<td>" + json[i].CL400 + "</td>");
+    tr.append("<td>" + json[i].CL500 + "</td>");
+    tr.append("<td>" + json[i].CL600 + "</td>");
+    tr.append("<td>" + json[i].CL700 + "</td>");
+    tr.append("<td>" + json[i].CL800 + "</td>");
+    tr.append("<td>" + json[i].CL900 + "</td>");
+    tr.append("<td>" + json[i].CL1000 + "</td>");
+    $('table').append(tr);
 
-date.ready(function () {
-    $.getJSON(url,
-        function (json) {
-            var tr;
-            for (var i = 0; i < json.length; i++) {
-                tr = $('<tr/>');
-                tr.append("<td>" + json[i].User_Name + "</td>");
-                tr.append("<td>" + json[i].score + "</td>");
-                tr.append("<td>" + json[i].team + "</td>");
-                $('table').append(tr);
-            }
-        });
+}
 });
